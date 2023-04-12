d872b79d2a0cfd146dfbce3b53eaf84d321babe0
gumbbbb <87250855+gumbbbb@users.noreply.github.com>
2023-04-13 01:27:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Bạn uii!',
+    introDesc: `Trái đất có 7 tỉ ngừi
+    Đố bạn biết ai yêu Bích Anh nhấcc`,
+    btnIntro: 'Lets find outtt',
+    title: 'Yêu Bích Anh nhất',
+    desc: 'Đố biếc luông :3333 ',
+    btnYes: 'Tứn Anh chứ ai :333',
+    btnNo: 'Hồng Anh và Bông',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Tuấn Anh chỉ yêu mỗi bạn thôiiiiiiiiiii',
+    btnReply: 'Chịuuuuuuu',
+    reply: 'Tại dì Bích Anh xinh gái tốt bụng đáng iu và vô vàn lí do khác chứ shao nựa <33333333',
+    mess: 'Chúc Bích Anh tuổi 19 ngày càng xinh đẹp, giỏi giang và yêu thương tui nhiều hơn nhaa',
+    messDesc: 'Hope u like this :3',
+    btnAccept: 'I love youuuuuuuu ',
+    messLink: 'https://m.me/as.cacf' 
+}
